2c4dcb65ea35de6a71a361f3e8967c337cf4e044
Guilherme Morais <guilherme.morais0111@gmail.com>
2022-12-22 22:20:57 -0300

    refact: alter env

--- src/controllers/urls/getShortURL.js
+++ src/controllers/urls/getShortURL.js
@@ -8,8 +8,8 @@ export async function getShortURL(req,res) {
             return res.sendStatus(404);
         }
         await incrementVisitCount(shortUrl, results.rows[0].visitCount+1);
-        // return res.status(200).redirect(results.rows[0].url);
-        return res.status(200).send(results.rows[0].url);
+        return res.status(200).redirect(results.rows[0].url);
+        // return res.status(200).send(results.rows[0].url);
     } catch(err) {
         console.log(err);
         return res.sendStatus(500);
